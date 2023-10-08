3e0967e5f11d726993821a4f64c4c9a7df2cbbc6
ISILV125 <igonildo7@gmail.com>
2023-10-08 20:07:46 -0300

    Added weapon type enumeration

--- Source/Shooter/Weapon.h
+++ Source/Shooter/Weapon.h
@@ -9,6 +9,17 @@
 /**
  * 
  */
+
+UENUM(BlueprintType)
+enum class EWeaponType :uint8 {
+	EWT_SubmachineGun UMETA(DisplayName = "SubmachineGun"),
+	EWT_AssaultRifle UMETA(DisplayName = "AssaultRifle"),
+	
+
+	EAT_MAX UMETA(DisplayName = "DefaultMAX")
+};
+
+
 UCLASS()
 class SHOOTER_API AWeapon : public AItem
 {
@@ -35,9 +46,14 @@ private:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
 		int32 Ammo = 0;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+		EWeaponType WeaponType = EWeaponType::EWT_SubmachineGun;
+
 public:
 
 	FORCEINLINE int32 GetAmmo()const { return Ammo; };
+
+	FORCEINLINE EWeaponType GetWeaponType()const { return WeaponType; };
 
 	void DecrementAmmo();
 	
